3ebb79c0373d945a8c57659a9af1f840a6fd4321
Masud <wubetumasud@gmail.com>
2025-12-02 17:52:19 +0300

    Schema

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/playground')
     .then(() => console.log('connected to mongoDB...'))
     .catch(err => console.error('could not connect to mogoDB...', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
