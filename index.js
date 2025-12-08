5f59451147643ca55d4ed4a56690ec25f947a592
Masud <wubetumasud@gmail.com>
2025-12-08 11:09:12 +0300

    Updating a document - query first approch

--- index.js
+++ index.js
@@ -50,4 +50,15 @@ async function getCourses() {
     console.log(courses);
 }
 
-getCourses();
+async function updateCourse(id) {
+   const course = await Course.findById(id);
+   if(!course) return;
+
+   course.isPublished = true;
+   course.author = 'Another Author';
+
+   const result = await course.save();
+   console.log(result);
+}
+
+updateCourse('6930857c8900f36e07f87fa4');
