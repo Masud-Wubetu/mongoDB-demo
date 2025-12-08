1f65240dc7b42d294c97ea2bec799e723f96e69f
Masud <wubetumasud@gmail.com>
2025-12-08 10:05:48 +0300

    solution1

---
+++ solution1.js
@@ -0,0 +1,27 @@
+const moongose = require('mongoose');
+
+moongose.connect('mongodb://localhost/mongo-exercises');
+
+const courseSchema = new moongose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = moongose.model('Course', courseSchema);
+async function getCourses() {
+    return await Course
+        .find({ isPublished: true, tags: 'backend' })
+        .sort({ name: 1 })
+        .select({ name: 1, author: 1 })
+}
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run()
