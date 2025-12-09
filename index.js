cf6385263e2c4847bafcb6032876de3b81243d14
Masud <wubetumasud@gmail.com>
2025-12-09 09:05:29 +0300

    Validation Error object

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     const course = new Course({
         name: 'Angular Tutorial',
-        category: 'web',
+        category: '-',
         author: 'Masud',
         tags: null,
         isPublished: true,
@@ -60,7 +60,9 @@ async function createCourse() {
         console.log(result);
     }
     catch(ex) {
-        console.log(ex.message);
+        for(field in ex.errors) 
+            console.log(ex.errors[field].message);
+
     }
     
 }
